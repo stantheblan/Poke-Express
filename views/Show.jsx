c52d4bf3300efb945e9075c95d7924fc10a22b66
Stan <stantonmoore42@gmail.com>
2022-03-09 14:27:55 -0500

    Created show views of each pokemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -4,7 +4,10 @@ class Show extends React.Component {
         let { pokemon } = this.props
         return (
             <div>
-                <h1>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>
+                <h1>Gotta Catch 'Em All</h1>
+                <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+                <img src = {`${pokemon.img}.jpg`}></img><br/>
+                <a href = {'/pokemon'}>Back</a>
             </div>
         )
     }
