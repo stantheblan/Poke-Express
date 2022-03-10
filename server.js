4e0853a5487cf24b10759477c28c5dad443efcd7
Stan <stantonmoore42@gmail.com>
2022-03-10 14:07:22 -0500

    push before heroku

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ const pokemon = require('./models/pokemon.js')
 const app = express()
 const PORT = process.env.PORT || 3001;
 
+// deploy to heroku =============================================
+
 app.use(express.urlencoded({extended:true}))
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+app.get('/pokemon/seed/', (req, res) => {
+    pokemon.create(
+        [{name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
+        {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
+        {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
+        {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
+        {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
+        {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
+        {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}],
+        (er, data) => {res.redirect('/pokemon')});
+}) 
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
@@ -20,7 +34,8 @@ app.get('/pokemon', (req, res) => {
         res.render('Index', {
           pokemon: allPoke
         })
-})}) 
+    })
+}) 
 
 app.get('/pokemon/new', (req, res) => {
     res.render('new')
